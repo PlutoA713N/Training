88f93ea8e4b8de6402daeb6823f41a131033bbad
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-19 19:26:19 +0530

    testProblem3.js

--- test/testProblem3.js
+++ test/testProblem3.js
@@ -22,7 +22,7 @@ console.log( brands );
   'Suzuki',        'Toyota',
   'Volkswagen',    'Volvo'
 ]
-*/
+*/ 
 
 let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
 {"id":2,"car_make":"Mazda","car_model":"Miata MX-5","car_year":2001},

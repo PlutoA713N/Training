44cba4e6210c105319a5cfcee4affa0038dce4eb
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-23 15:42:57 +0530

    map.js

---
+++ JS Drills/Arrays/solution/map.js
@@ -0,0 +1,14 @@
+// This is a map function works similar to array.prototype.map
+// It doesnt consists of .this property
+// It works only for Arrays and the result will be returned in an Array.
+
+function map(array, callback){
+  var ar = [];
+    if( Array.isArray(array) ){
+        for(let index = 0; index < array.length; index++){
+           // Result of callback function will be stored in an array
+             ar.push( callback( array[index], index, array) );
+        }
+    }
+   return ar;
+}

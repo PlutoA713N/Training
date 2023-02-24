c24100edeed6187ca492e464a5665203aa568724
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 22:48:17 +0530

    Create testKeys.js

---
+++ JS Drills/Objects/test/testKeys.js
@@ -0,0 +1,12 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+// Check and use MDN as well
+
+
+function keys(obj) {
+    // Retrieve all the names of the object's properties.
+    // Return the keys as strings in an array.
+    // Based on http://underscorejs.org/#keys
+}

af12cde35f67e09dbedb489b318693188046feed
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 22:50:19 +0530

    Create testValues.js

---
+++ JS Drills/Objects/test/testValues.js
@@ -0,0 +1,11 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+// Check and use MDN as well
+
+function values(obj) {
+    // Return all of the values of the object's own properties.
+    // Ignore functions
+    // http://underscorejs.org/#values
+}

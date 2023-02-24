b96c7a120d607618e004844b2c6cf7c44f2efd83
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 14:29:04 +0530

    Create testMap.js

---
+++ JS Drills/Arrays/tests/testMap.js
@@ -0,0 +1,24 @@
+const items = [1, 2, 3, 4, 5, 5]; // use this array to test your code.
+/*
+    Complete the following functions.
+    These functions only need to work with arrays.
+    A few of these functions mimic the behavior of the `Built` in JavaScript Array Methods.
+    The idea here is to recreate the functions from scratch BUT if you'd like,
+    feel free to Re-use any of your functions you build within your other functions.
+    **DONT** Use for example. .forEach() to recreate each, and .map() to recreate map etc.
+    You CAN use concat, push, pop, etc. but do not use the exact method that you are replicating
+
+    Name your files like so:
+        each.js
+        testEach.js
+        map.js
+        testMap.js
+*/
+
+
+function map(elements, cb) {
+    // Do NOT use .map, to complete this function.
+    // How map works: Map calls a provided callback function once for each element in an array, in order, and functionructs a new array from the res .
+    // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
+    // Return the new array.
+}

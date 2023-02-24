8f19ab568571c6a8f0cae233b3016576c5477724
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 22:52:05 +0530

    Create testMapObject.js

---
+++ JS Drills/Objects/test/testMapObject.js
@@ -0,0 +1,10 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+// Check and use MDN as well
+
+function mapObject(obj, cb) {
+    // Like map for arrays, but for objects. Transform the value of each property in turn by passing it to the callback function.
+    // http://underscorejs.org/#mapObject
+}

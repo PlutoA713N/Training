2d88e47c8c79f23f34a87968fdd5be29de6ebd9b
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 14:33:52 +0530

    Create testFlatten.js

---
+++ JS Drills/Arrays/tests/testFlatten.js
@@ -0,0 +1,24 @@
+const items = [1, 2, 3, 4, 5, 5]; // use this array to test your code.
+/*
+    Complete the following functions.
+    These functions only need to work with arrays.
+    A few of these functions mimic the behavior of the `Built` in JavaScript Array Methods.
+    The idea here is to recreate the functions from scratch BUT if you'd like,
+    feel free to Re-use any of your functions you build within your other functions.
+    **DONT** Use for example. .forEach() to recreate each, and .map() to recreate map etc.
+    You CAN use concat, push, pop, etc. but do not use the exact method that you are replicating
+
+    Name your files like so:
+        each.js
+        testEach.js
+        map.js
+        testMap.js
+*/
+
+const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
+
+function flatten(elements) {
+    // Flattens a nested array (the nesting can be to any depth).
+    // Hint: You can solve this using recursion.
+    // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+}

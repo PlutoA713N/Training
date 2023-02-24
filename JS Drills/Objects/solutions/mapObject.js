4bff60913b52cdf4adb0d4d0cd11791e6d76b858
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 16:15:28 +0530

    Create mapObject.js

---
+++ JS Drills/Objects/solutions/mapObject.js
@@ -0,0 +1,19 @@
+// mapObject
+// It consists of a callback function and an Object.
+// With callback function, it traverses through the whole object values and applies callback function to each of the values.
+// It modifies the Original Object  and returns it.
+
+function mapObject( testObject, callback ){
+    
+    if(testObject.constructor == Object){
+        for( let key in testObject ){
+            testObject[key] = callback( testObject[key] )
+        }
+    }
+    
+    return testObject;
+}
+
+console.log( mapObject ( {start: 5, end: 12}, value => {
+   return  value * 5;
+}) ) // Output - { start: 25, end: 60 }

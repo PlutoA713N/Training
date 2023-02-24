2408a3c39d6e94a6ec9055a60af24c2e5c03ac49
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 14:32:39 +0530

    Create testFilter.js

---
+++ JS Drills/Arrays/tests/testFilter.js
@@ -0,0 +1,22 @@
+const items = [1, 2, 3, 4, 5, 5]; // use this array to test your code.
+/*
+    Complete the following functions.
+    These functions only need to work with arrays.
+    A few of these functions mimic the behavior of the `Built` in JavaScript Array Methods.
+    The idea here is to recreate the functions from scratch BUT if you'd like,
+    feel free to Re-use any of your functions you build within your other functions.
+    **DONT** Use for example. .forEach() to recreate each, and .map() to recreate map etc.
+    You CAN use concat, push, pop, etc. but do not use the exact method that you are replicating
+
+    Name your files like so:
+        each.js
+        testEach.js
+        map.js
+        testMap.js
+*/
+
+function filter(elements, cb) {
+    // Do NOT use .filter, to complete this function.
+    // Similar to `find` but you will return an array of all elements that passed the truth test
+    // Return an empty array if no elements pass the truth test
+}

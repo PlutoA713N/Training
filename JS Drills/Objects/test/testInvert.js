a03ca3cab320a6fdfc0ca99f44f0c4e8d3bff5e6
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 22:54:04 +0530

    Create testInvert.js

---
+++ JS Drills/Objects/test/testInvert.js
@@ -0,0 +1,13 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+// Check and use MDN as well
+
+
+
+function invert(obj) {
+    // Returns a copy of the object where the keys have become the values and the values the keys.
+    // Assume that all of the object's values will be unique and string serializable.
+    // http://underscorejs.org/#invert
+}

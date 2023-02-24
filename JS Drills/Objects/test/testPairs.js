3f1e5eeed0a8dca3167327126cc79b98d8094360
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 22:53:05 +0530

    Create testPairs.js

---
+++ JS Drills/Objects/test/testPairs.js
@@ -0,0 +1,10 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+// Check and use MDN as well
+
+function pairs(obj) {
+    // Convert an object into a list of [key, value] pairs.
+    // http://underscorejs.org/#pairs
+}

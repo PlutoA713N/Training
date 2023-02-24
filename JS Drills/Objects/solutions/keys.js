cf09aff29c768e58ab5fd129ea6411f797837f7b
PlutoA713N <107915143+PlutoA713N@users.noreply.github.com>
2023-02-24 15:26:46 +0530

    Create keys.js

---
+++ JS Drills/Objects/solutions/keys.js
@@ -0,0 +1,22 @@
+// const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; // use this object to test your functions
+
+// Object.keys() 
+// It is a function returns the all keys of an Object.
+// It returns the all the keys of an object in an Array , each key in a strimg format
+// Works only for Objects.
+
+function keys( testObject ){
+    const objectKeys = new Array();
+    
+    if( testObject.constructor == Object){
+       for(let key in testObject){
+       console.log(key)
+       objectKeys.push(key)  
+        }
+    return objectKeys;
+    }
+    
+    return undefined;
+}
+
+console.log( keys( testObject ) )
